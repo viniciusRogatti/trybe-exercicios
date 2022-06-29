280c5d173c950655f24f2a407bae24d5b506bd9f
vinicius <vinnyzsouza@gmail.com>
2022-06-29 12:30:22 -0300

    exercicio 9 dia/8-5

--- Fundamentos/Bloco 08/8.5/exercicios.js
+++ Fundamentos/Bloco 08/8.5/exercicios.js
@@ -120,11 +120,21 @@ console.log(shipLength(ships[2])); // 'Yamato is 256 meters long' */
 // Dica: use default params.
 // escreva greet abaixo
 const greet = (name, msg = 'Hi') => `${msg} ${name}`; 
-
+/* 
 console.log(greet('John')) // 'Hi John'
 console.log(greet('John', 'Good morning')) // 'Good morning John'
-console.log(greet('Isabela', 'Oi')) // 'Oi Isabela'
+console.log(greet('Isabela', 'Oi')) */ // 'Oi Isabela'
 
+// [9].Existe um objeto yearSeasons que representam as estações do ano. Cada estação é uma chave deste objeto e o valor de cada chave é um array de strings, representando os meses daquela estação. A partir deste objeto, desestruture as estações do ano e espalhe-as em um array de meses do ano.
+// Dica: use object destructuring e spread operator.
+// OBS: As estações do ano aqui descritas têm base nas estações do hemisfério norte.
+const yearSeasons = {
+  spring: ['March', 'April', 'May'],
+  summer: ['June', 'July', 'August'],
+  autumn: ['September', 'October', 'November'],
+  winter: ['December', 'January', 'February'],
+};
 
-
-
+const {spring, summer, autumn, winter} = yearSeasons;
+const months = [...spring, ...summer, ...autumn, ...winter];
+console.log(months);
