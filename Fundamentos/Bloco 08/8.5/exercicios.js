dc2cbc2752a76f3c614cbaa3e3109a0fd7b5e9d3
vinicius <vinnyzsouza@gmail.com>
2022-06-29 11:52:40 -0300

    exercicio 6 dia/8-5

--- Fundamentos/Bloco 08/8.5/exercicios.js
+++ Fundamentos/Bloco 08/8.5/exercicios.js
@@ -79,4 +79,13 @@ const people = [
 const myList = [1, 2, 3];
 // escreva swap abaixo
 const swap = ([a,b,c]) => [c,b,a];
-console.log(swap(myList));
+// console.log(swap(myList));
+
+// [6].Suponha que você esteja lidando com carros e, da forma como o problema lhe foi entregue, cada carro é modelado como um array. Porém, essa modelagem está baixo nível. Cria uma função toObject que, dada uma lista, retorna um objeto representando o carro:
+//Dica: use array destructuring e abbreviation object literal.
+const palio = ['Palio', 'Fiat', 2019];
+const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
+const chiron = ['Chiron', 'Bugatti', 2016];
+
+const toObject = ([Nome,Marca,Ano]) => ({ Nome, Marca, Ano,});
+console.log(toObject(palio));
