8f9e124147fd151c2c084580262f7ed65ce49707
vinicius <vinnyzsouza@gmail.com>
2022-06-29 11:15:18 -0300

    exercicio 4 dia/8-5

--- Fundamentos/Bloco 08/8.5/exercicios.js
+++ Fundamentos/Bloco 08/8.5/exercicios.js
@@ -1,12 +1,10 @@
 // [1].Dado o código abaixo, complete-o de forma que seja impressa a área dos 3 retângulos. O código deve retornar em sequência 2, 15 e 54.
 // Dica: use spread operator.
 const rectangleArea = (width, height) => width * height;
-
 const rectangle1 = [1, 2];
 const rectangle2 = [3, 5];
 const rectangle3 = [6, 9];
 const rectangles = [rectangle1, rectangle2, rectangle3];
-
 rectangles.forEach((rectangle) => {
     rectangleArea(...rectangle);    
 });
@@ -14,8 +12,7 @@ rectangles.forEach((rectangle) => {
 // [2].Crie uma função sum que dado um número variável de elementos retorna a soma desses elementos.
 // Dica: use parâmetro rest
 const sum = (...numbers) => numbers.reduce((acc, curr) => acc + curr);
-
-console.log(sum(1,2,3,4,5,6,7,8));
+// console.log(sum(1,2,3,4,5,6,7,8));
 
 // [3].Escreva a função personLikes, que dado um objeto de parâmetro que representa uma pessoa, retorna todos os gostos daquela pessoa, conforme mostrado abaixo:
 // Dica: use object destructuring.
@@ -39,5 +36,40 @@ const alex = {
     return `${name} is ${age} years old and likes ${likes.join(', ')}.`;
   }
   
-  console.log(personLikes(alex)); // 'Alex is 26 years old and likes fly fishing.'
-  console.log(personLikes(gunnar)); // 'Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.'
+  //console.log(personLikes(alex)); // 'Alex is 26 years old and likes fly fishing.'
+  //console.log(personLikes(gunnar)); // 'Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.'
+
+
+// [4].Escreva uma função filterPeople que, dada uma lista de pessoas, retorna todas as pessoas australianas que nasceram no século 20:
+// Dica: use object destructuring.
+const people = [
+    {
+      name: 'Nicole',
+      bornIn: 1992,
+      nationality: 'Australian',
+    },
+    {
+      name: 'Harry',
+      bornIn: 2008,
+      nationality: 'Australian',
+    },
+    {
+      name: 'Toby',
+      bornIn: 1901,
+      nationality: 'Australian',
+    },
+    {
+      name: 'Frida',
+      bornIn: 1960,
+      nationality: 'Dannish',
+    },
+    {
+      name: 'Fernando',
+      bornIn: 2001,
+      nationality: 'Brazilian',
+    },
+  ];  
+  // escreva filterPeople abaixo  
+
+  const filterPeople = (array) => array.filter(({bornIn, nationality }) => bornIn > 1900 && bornIn < 2001 && nationality === 'Australian');  
+  console.log(filterPeople(people));
