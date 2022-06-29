49e39fae4bcf0677527aae9327f8140655879af3
vinicius <vinnyzsouza@gmail.com>
2022-06-29 10:30:47 -0300

    exercicio 2  dia/8-5

--- Fundamentos/Bloco 08/8.5/exercicios.js
+++ Fundamentos/Bloco 08/8.5/exercicios.js
@@ -1,6 +1,6 @@
 // [1].Dado o código abaixo, complete-o de forma que seja impressa a área dos 3 retângulos. O código deve retornar em sequência 2, 15 e 54.
 // Dica: use spread operator.
-const rectangleArea = (width, height) => console.log(width * height);
+const rectangleArea = (width, height) => width * height;
 
 const rectangle1 = [1, 2];
 const rectangle2 = [3, 5];
@@ -10,3 +10,9 @@ const rectangles = [rectangle1, rectangle2, rectangle3];
 rectangles.forEach((rectangle) => {
     rectangleArea(...rectangle);    
 });
+
+// [2].Crie uma função sum que dado um número variável de elementos retorna a soma desses elementos.
+// Dica: use parâmetro rest
+const sum = (...numbers) => numbers.reduce((acc, curr) => acc + curr);
+
+console.log(sum(1,2,3,4,5,6,7,8));
