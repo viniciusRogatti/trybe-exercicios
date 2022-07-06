72bcf78ad32b3373b31762c46346f6a77b954ca1
vinicius <vinnyzsouza@gmail.com>
2022-07-06 19:07:08 -0300

    exercicios dia/9-1 exercicio 2

--- Desenvolvimento Front-end/Bloco 09/9-1/exercicios.js
+++ Desenvolvimento Front-end/Bloco 09/9-1/exercicios.js
@@ -1,6 +1,6 @@
 // [1] 3 - A função getPlanet abaixo imprime o planeta Marte de forma síncrona. Modifique getPlanet, de forma que Marte seja impresso assincronamente, depois de 4 segundos.
 
-const getPlanet = () => {
+/* const getPlanet = () => {
   setTimeout(() => {
     const mars = {
       name: "Mars",
@@ -14,4 +14,20 @@ const getPlanet = () => {
   }, 4000)
 };
 
-getPlanet(); // imprime Marte depois de 4 segundos
+getPlanet(); // imprime Marte depois de 4 segundos */
+
+// [2] - Suponha que você precise simular uma mensagem enviada do robô Curiosity de Marte para a Terra. O Curiosity envia para a Terra a temperatura atual em Marte, gastando um tempo variável de até 5 segundos para que termine o envio. Crie a função sendMarsTemperature, que imprime a temperatura em Marte.
+
+const messageDelay = () => Math.floor(Math.random() * 5000);
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+const sendMarsTemperature = (param) => {
+  setTimeout(() => {
+    console.log(`Mars Temperature is ${param} degrees Celsius`)
+  },messageDelay());
+}
+// crie a função sendMarsTemperature abaixo
+
+sendMarsTemperature(getMarsTemperature()); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
